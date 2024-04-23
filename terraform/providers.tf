8b2ead72df17f41fb686a96331855a9e821e07bb
Matt Williamson <matt.williamson@cdw.com>
2024-04-23 14:56:18 -0500

    Provider updates

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,11 +15,11 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 provider "helm" {
